a9c7f7e90d51e8e4ff49b63e2beedeba61288e8a
jinlee4140 <jinlee4140@gmail.com>
2015-10-02 13:17:51 -0400

    able to extract all of the emails from the homepage and its links

--- code.py
+++ code.py
@@ -3,22 +3,31 @@ import re
 import urllib2
 from sys import argv
 
+def extract_emails_from_text(text):
+  addresses = list()
+  for word in text.split(' '):
+    addresses += re.findall(r'[a-z]+[A-Za-z0-9._%+-]+@\S+\.com', word)
+    
+  return addresses
+  
+
 script, filename = argv
 
 html = urllib2.urlopen("http://" + filename).read()
-
 soup = BeautifulSoup(html, "html.parser") #Don't know why we need "html.parser". I am just putting this to get rid of the warning sign
-
 all_text = soup.get_text(' ')
-all_words = all_text.split(' ')
+email_addresses = extract_emails_from_text(all_text)
 
-# print all_words
-email_addresses = list()
-
-for word in all_words:
-  email_addresses += re.findall(r'\S+@\S+.com|net', word)
+for link in soup.find_all('a'):
+    try:
+      html = urllib2.urlopen(link.get('href')).read()
+      soup = BeautifulSoup(html, "html.parser")
+      text = soup.get_text(' ')
+      email_addresses += extract_emails_from_text(text)
+    except:
+      pass
 
 print "These are the following e-mails that were retrieved in this following site: %s" %filename
 
-# print set(email_addresses)
-print set(email_addresses)
+for email in set(email_addresses):
+  print email
