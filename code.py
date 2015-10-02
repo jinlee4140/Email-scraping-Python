aec8ee7b709b6e82f491b216ee8dd036a74ebe26
jinlee4140 <jinlee4140@gmail.com>
2015-10-02 13:16:10 -0400

    using some libraries to scrape emails

--- code.py
+++ code.py
@@ -1,15 +1,29 @@
-import urllib2
-import re
 from bs4 import BeautifulSoup
+import re
+import urllib2
+from sys import argv
+
+script, filename = argv
+
+
+html = urllib2.urlopen("http://" + filename).read()
+
+
+soup = BeautifulSoup(html, "html.parser") #Don't know why we need "html.parser". I am just putting this to get rid of the warning sign
+
+text = soup.get_text()
+
+print text
+
+results = soup('a')
+
+print "These are the following e-mails that were retrieved in this following site: %s" %filename
+for tag in results:
+	if re.match(r'\S+@\S+.com|net', tag['href']):
+		print tag['href']
 
 
 
-html = urllib2.urlopen('http://www.cogolabs.com').read()
+# '\S+@\S+\.(?:(?:com)|(?:net))'
 
-print html
-
-# Extract all email addresses present in html
-
-# 1. Regex matching
-m = re.findall(r'\S+@\S+\.(?:(?:com)|(?:net))', html)
-print m
+# '[^@]+@[^@]+\.[^@]+'
